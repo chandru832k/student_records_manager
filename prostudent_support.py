0be56429c9c1200997b3604804ff928ef806954a
chandru832k <62912759+chandru832k@users.noreply.github.com>
2021-07-22 23:46:27 +0530

    comments removed

--- prostudent_support.py
+++ prostudent_support.py
@@ -7,28 +7,27 @@ except ImportError:
 
 try:
     import ttk
-    py3 = False
+    py3=False
 except ImportError:
     import tkinter.ttk as ttk
-    py3 = True
+    py3=True
 
 def set_Tk_var():
     global selectedButton
-    selectedButton = tk.IntVar()
+    selectedButton=tk.IntVar()
     global spinbox
-    spinbox = tk.StringVar()
+    spinbox=tk.StringVar()
 
 def init(top, gui, *args, **kwargs):
     global w, top_level, root
-    w = gui
-    top_level = top
-    root = top
+    w=gui
+    top_level=top
+    root=top
 
 def destroy_window():
-    # Function which closes the window.
     global top_level
     top_level.destroy()
-    top_level = None
+    top_level=None
 
 if __name__ == '__main__':
     import prostudent
